da0256e855f2f21218427e3535c53924532ed1b6
pipe.zkk <pipe.zkk@alibaba-inc.com>
2024-07-16 16:43:03 +0800

    format: compose

--- compose.mjs
+++ compose.mjs
@@ -4,13 +4,13 @@
 // 支持同步插件调用
 //
 export default function (middleware, options = {}) {
-  const { sync = false } = options
+  const { sync = false } = options;
 
   if (!Array.isArray(middleware))
-    throw new TypeError('Middleware stack must be an array!')
+    throw new TypeError('Middleware stack must be an array!');
   for (const fn of middleware) {
     if (typeof fn !== 'function')
-      throw new TypeError('Middleware must be composed of functions!')
+      throw new TypeError('Middleware must be composed of functions!');
   }
 
   /**
@@ -21,49 +21,48 @@ export default function (middleware, options = {}) {
 
   return function (context, next) {
     // last called middleware #
-    let index = -1
-    return dispatch(0)
+    let index = -1;
+    return dispatch(0);
     function dispatch (i) {
       if (i <= index) {
-        if (sync) return new Error('next() called multiple times')
-        return Promise.reject(new Error('next() called multiple times'))
+        if (sync) return new Error('next() called multiple times');
+        return Promise.reject(new Error('next() called multiple times'));
       }
-      index = i
-      let fn = middleware[i]
+      index = i;
+      let fn = middleware[i];
 
       // 最后一个函数，一般就是被注解的本事函数，不会调用 next
-      if (i === middleware.length) fn = next
+      if (i === middleware.length) fn = next;
       if (!fn) {
-        if (sync) return null
-        return Promise.resolve()
+        if (sync) return null;
+        return Promise.resolve();
       }
-
 
       try {
         // 为了不改变扩展点函数的写法，最后一个函数，按打平参数传入
         if (i === middleware.length - 1) {
           if (sync) {
             // Context 中约定了 self 和 args 两个属性
-            return fn.call(context.self, ...context.args)
+            return fn.call(context.self, ...context.args);
           }
-          return Promise.resolve(fn.call(context.self, ...context.args))
+          return Promise.resolve(fn.call(context.self, ...context.args));
         }
 
         // 其他中间件函数，按 koa 的洋葱模型进行调用
         if (sync) {
-          return fn.call(context.self, context, dispatch.bind(null, i + 1))
+          return fn.call(context.self, context, dispatch.bind(null, i + 1));
         }
         // 添加 this 指向
         // 如果是箭头函数，就用 context.self 来获取
         return Promise.resolve(
           fn.call(context.self, context, dispatch.bind(null, i + 1))
-        )
+        );
       } catch (err) {
         if (sync) {
-          throw err
+          throw err;
         }
-        return Promise.reject(err)
+        return Promise.reject(err);
       }
     }
-  }
+  };
 }
